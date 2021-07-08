fae1f54368124d1e3bed1f2c30cfbbd5324d5af5
kamikazekalen <kalen.macmillan@gmail.com>
2021-07-08 15:09:54 -0400

    I finished Paparazzi Part 1.
    I finished Paparazzi Part 1, if steering is
    valid, turn the wheels, also if motor is valid, move the wheels.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    CameraCalculations cameraCalc;
+
+    public Transform target;
+
+    public int playeLayerNumber = 9;
+
+    public float maxDistance = 4.0f;
+    public float minDistance = 2.0f;
+    public float minHeight = 1.0f;
+
+    public float distanceOffset;
+    public float finalDistance;
+
+    public Vector2 cameraSpeed = new Vector2(3.0f, 1.0f);
+    public Vector2 cameraYLimits = new Vector2(5.0f, 60.0f);
+
+    public bool resetCamera = true;
+
+    public float x = 0.0f;
+    public float y = 0.0f;
+    public float yOffset;
+    public float newPos;
+
+    private Quaternion rotation;
+    private Vector3 position;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Axle class that defines what gose into our axles
+[System.Serializable]
+public class Axle
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
+//Controllor class that moves the kart wheels
+public class Controller : MonoBehaviour
+{
+    //List of axles and motor/steering values for kart
+    public List<Axle> axles;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
+    //Fixed update that runs the Physics and input
+    public void FixedUpdate()
+    {
+        //Get the motor and steering input WASD
+        float motor = maxMotorTorque * Input.GetAxis("Vertical"); //W-Forwards, S-Backwords
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal"); //A-Left, D-Right
+        //Foreach loop that runs through all the axles in the list
+        foreach (Axle axle in axles)
+        {
+            //If motor is valid, move the wheels
+            if (axle.motor)
+            {
+                axle.leftWheel.motorTorque = motor;
+                axle.rightWheel.motorTorque = motor;
+            }
+            //If steering is valid, turn the wheels
+            if (axle.steering)
+            {
+                axle.leftWheel.steerAngle = steering;
+                axle.rightWheel.steerAngle = steering;
+            }
+        }
+    }
+}
