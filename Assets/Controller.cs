2546e26e60015154d2f77e2ef524328cb9c5d31b
kamikazekalen <kalen.macmillan@gmail.com>
2021-08-27 13:53:49 -0400

    I finished CH 2 and I finished a quiz
    My dad and I made 2 things, after I am
    done with my game I and dad will try to
    make it so that I go on fire if I am on the
    lava.

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -19,11 +19,19 @@ public class Controller : MonoBehaviour
     public float maxMotorTorque;
     public float maxSteeringAngle;
 
+    // Water
+    private float waterSlowdown = 1.0f;
+    private const float onWater = 0.1f;
+
+    // SpeedBooster
+    private float speedBooster = 1.0f;
+    private const float onSpeedBooster = 20.0f;
+
     //Fixed update that runs the Physics and input
     public void FixedUpdate()
     {
         //Get the motor and steering input WASD
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");//W-Forwards, S-Backwords
+        float motor = GetSpeed(Input.GetAxis("Vertical"));//W-Forwards, S-Backwords
         float steering = maxSteeringAngle * Input.GetAxis("Horizontal"); //A-Left, D-Right
         //Foreach loop that runs through all the axles in the list
         foreach (Axle axle in axles)
@@ -41,5 +49,59 @@ public class Controller : MonoBehaviour
                 axle.rightWheel.steerAngle = steering;
             }
         }
+    }
+
+    private float GetSpeed(float verticalAxis)
+    {
+        return maxMotorTorque * verticalAxis * waterSlowdown * speedBooster;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Water")
+        {
+            waterSlowdown = onWater;
+        }
+        if (collision.gameObject.name == "Terrain") {
+            waterSlowdown = 1.0f;
+            speedBooster = 1.0f;
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // Doesn't work with wheel colliders?
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        waterSlowdown = collision.collider.tag == "Water" ? onWater : 1.0f;
+    }
+
+
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "SpeedBooster")
+        {
+            speedBooster = onSpeedBooster;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "SpeedBooster")
+        {
+            speedBooster = 1.0f;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        speedBooster = other.tag == "SpeedBooster" ? onSpeedBooster : 1.0f;
     }
 }
