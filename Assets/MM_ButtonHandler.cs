c9b5ca31c66af5164ee4760afeff7242c28006f5
kamikazekalen <kalen.macmillan@gmail.com>
2021-10-22 11:20:42 -0400

    I finished press some buttons! yay!
    I finished press some buttons and I
    made my play button go into the game!
    YAY!!!

---
+++ Assets/MM_ButtonHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MM_ButtonHandler : MonoBehaviour
+{
+    public Button playButton, quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+        playButton.onClick.AddListener(delegate { NextScene(); });
+        quitButton.onClick.AddListener(delegate { QuitGame(); });
+    }
+
+    void NextScene()
+    {
+        SceneManager.LoadScene("CharacterSelect");
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}
